432df670ab09ac006ab706c745f42cf839389c31
Brian Rosner <brosner@gmail.com>
2008-12-29 00:43:08 -0700

    Updated the version.

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ copyright = u'2008, Brian Rosner'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.0'
 
